616c794726722a13570b6109194e03aeae42cf56
petermkr <mail@peter-kroiss.de>
2019-02-13 23:34:27 +0100

    Add first unit test for backend

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,8 +2,11 @@ from app import app
 from app.models import License
 from flask import jsonify
 
+def serialize_list(input_list):
+    return [elem.serialize() for elem in input_list]
+
 
 @app.route('/api/licenses')
 def get_license():
     licenses = License.query.all()
-    return jsonify([lic.serialize() for lic in licenses])
+    return jsonify(serialize_list(licenses))

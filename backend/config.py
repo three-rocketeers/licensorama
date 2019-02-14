fc65f630021132f25b46d399de93251901a045b2
petermkr <mail@peter-kroiss.de>
2019-02-14 23:40:14 +0100

    Repair test bug

--- backend/config.py
+++ backend/config.py
@@ -2,15 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-b = 2
-
-if b == 1:
-    a = 1
-elif b == 2:
-    a = 2
-elif b == 1:            # Noncompliant
-    a = 3
-
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')

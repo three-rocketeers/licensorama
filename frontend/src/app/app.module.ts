e6de491235d9bc68a6655384e23d3e44ec5647ac
Ben Stoffelen <benstoffelen@gmail.com>
2019-04-01 22:19:43 +0200

    [BST] Initial Card Design

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatButtonModule} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { AdminTableComponent } from './admin-table/admin-table.component';
 import { AdminTableRowComponent } from './admin-table-row/admin-table-row.component';
@@ -14,7 +16,11 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]

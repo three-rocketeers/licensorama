5c8ee3267472aa4ae507d50f63239bfd5a660449
Ben Stoffelen <benstoffelen@gmail.com>
2019-04-04 20:56:44 +0200

    [BST] Frontend first Version with all dummy Data.

--- frontend/src/app/catalog-card/catalog-card.component.ts
+++ frontend/src/app/catalog-card/catalog-card.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogCardComponent implements OnInit {
 
-  constructor() { }
+  avatar_img = "/assets/images/jetbrains.png";
+  box_img = "assets/images/intellij.png";
+  box_alt = "Intellij Boxshot";
+  description = "Every aspect of IntelliJ IDEA is specifically designed to maximize developer productivity.\n" +
+    "      Together, the powerful static code analysis and ergonomic design make development not only productive but also\n" +
+    "      an enjoyable experience.";
+  title = "Intelli-J";
+  type = "Programming";
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
